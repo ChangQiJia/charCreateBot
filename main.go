6eb466692a529308b22a6db5c819025f5cd511cc
unknown <qijiachang@hotmail.com>
2019-12-31 19:03:22 +0800

    ordered roll

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"os"
 	"log"
 	"fmt"
-    tb "gopkg.in/tucnak/telebot.v2"	
+	tb "gopkg.in/tucnak/telebot.v2"	
+	"math/rand"
+	"time"
+	"strconv"
 )
 
 func main(){
@@ -40,6 +43,40 @@ func main(){
 		b.Send(m.Sender, "Hi!")
 	})
 
+	b.Handle("/order", func(m *tb.Message) {
+		orderedRoll(b, m)
+	})
+
 	b.Start()
 	
+}
+
+func orderedRoll(b *tb.Bot, m *tb.Message){
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	for i:= 0; i<6 ;i++{
+		eachScore := 0
+		min := 10
+		outputStr := "("
+
+		for roll:=0 ; roll < 4; roll++{
+			oneDsix := rand.Intn(6)+1
+			if (oneDsix < min){
+				min = oneDsix
+			}
+			eachScore += oneDsix
+
+			outputStr += strconv.Itoa(oneDsix)
+
+			if (roll < 3){
+				outputStr += " + "
+			}else{
+				outputStr += ") = "
+				eachScore -= min
+				outputStr += strconv.Itoa(eachScore)
+			}
+		}
+
+		b.Send(m.Sender, outputStr)
+	}
 }
