32fe17c000393680af790d9bf7069c7d9d63df65
unknown <qijiachang@hotmail.com>
2019-12-31 18:43:56 +0800

    Adding print

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"os"
 	"log"
+	"fmt"
     tb "gopkg.in/tucnak/telebot.v2"	
 )
 
 func main(){
 	
+	fmt.Println ("~~ Starting App")
+
 	var (
         port      = os.Getenv("PORT")       // sets automatically
         publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
@@ -22,10 +25,13 @@ func main(){
 	pref := tb.Settings{
         Token:  token,
         Poller: webhook,
-    }
+	}
+	
+	fmt.Println ("~~ Creating bot")
 
     b, err := tb.NewBot(pref)
     if err != nil {
+		fmt.Println ("~~ Oh No!")
         log.Fatal(err)
 	}
 
